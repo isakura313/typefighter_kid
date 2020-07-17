18f1ad7fa052d4cf95e4a529c4200ad736d47fe2
Paul <isakura313@gmail.com>
2020-07-17 13:41:07 +0300

    dfd

---
+++ js/code.js
@@ -0,0 +1,115 @@
+const symbol_colors = [
+  "is-primary", //стиль для легкой зеленой кнопки
+  "is-info", // стиль для синей кнопки 
+  "is-link", // стиль для синей вроде кнопки
+  "is-warning", // стиль для красной кнопки
+  "is-success", // стиль для успешной синей кнопки
+  "is-danger", // для прям такой очень красной
+  "is-primary is-light is-outlined", // вносим небольшое разнообразие
+  "is-link is-light is-outlined", // светлая кнопка только обведенная
+  "is-success is-light is-outlined",  // белый шрифт обведенная
+]; // здесь у нас просто находится набор стилей из bulma, который можно использовать, чтобы у нас кнопку были различных стилей
+
+
+// здесь у нас представлена наша информация в виде объекта, который состоит из массивов
+const level_info = [
+  {
+    symbols: ["j", "f", "k", "d", " "],
+  },
+  {
+    symbols: ["f", "d", "s", " ", "j", "k", "l", ";"],
+  },
+  {
+    symbols: ["v", "i", "d", ";", " ", "d", "e", "f"],
+  },
+];
+
+//получаем все звуки, которые нам нужны
+var number_of_level = 0; // по дефолту нас будет запускаться уровень номер 0
+var error_sound = new Audio("sounds/error_sound.wav"); //здесь у нас просто будет звук ошибки
+var fail_sound = new Audio("sounds/fail_sound.wav"); //звук неудачи в нашей игре
+var press_sound = new Audio("sounds/press_sound.wav"); //звук, который издается при нажатии на клавиши
+var succes_sound = new Audio("sounds/success_sound.mp3"); // звук успешного прохождения уровни
+var main_theme = new Audio("sounds/Main_theme_2.mp3"); //звук игры, который играет в главном меню
+
+let modal = document.querySelector(".modal"); //модальное окно, которое у нас появляется в конце игры, чтобы продемонстрировать нам результаты
+var target_error = document.querySelector(".target_error"); //ряд в таблице, в котором мы будем писать наши данные о том, кто у нас что смог достигнуть в навыких печати
+let error_panel = document.querySelector(".error-panel"); // панель, в которую мы будем добалять значение, если кто-то будет ошибаться
+let promo = document.querySelector(".promo"); // промо надпись, которая нам в игре самой не нужна, нужна только, чтобы игрок сам понял, что происходит в игре
+let begin = document.querySelector(".begin"); // здесь у нас надпись, которая приглашает пользователя нажать enter для начала игры. Потом она у нас должна пропасть
+let progress = document.getElementById("prog"); // здесь прогресс ошибок пользователя
+let buttons = document.querySelector(".buttons"); // элемент в который мы будем писать наши буковки
+let modal_close = document.querySelector(".modal-close"); //кнопка, при нажатии на которую у нас будет закрываться модальное окно 
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)); // функция, которая указывает от какого до какого она будет выкидывать рандомные целые числа
+}
+
+//теперь нужно отрисовать наши буковки
+
+document.addEventListener('keydown', StartGame, {
+  once:true, //после одного нажатия удалется слушатель
+});
+
+function StartGame(e){
+  if (e.key = "Enter"){
+    error_panel.classList.remove("is-hidden") // показываем панель ошибок сверху
+    press_sound.play() 
+    begin.remove()
+    mainGame() //запуск функции основной игры
+  }
+
+function drawBoard(info){
+  main_theme.play() // запускаем основной саундтрек игры
+  let str_arr = level_info[number_of_level].symbols; // получаем информацию, какие буквы рисовать
+  let col_arr = symbol_colors;
+
+  for (let i = 0; i < 20; i++) {
+    let rand = getRandomInt(str_arr.length) //получаем случайных индекс буквы
+
+    buttons.insertAdjacentHTML(
+      'afterbegin',
+      `<button class="game-button button is-large ${col_arr[rand]}" id='${str_arr[rand]}'> ${str_arr[rand]}  </button> `
+    );
+    
+  }
+}
+
+function mainGame(){
+  drawBoard(level_info)
+  document.addEventListener("keydown", press)
+}
+
+}
+
+
+
+
+
+
+
+function showResult(target_El, content) {
+  main_theme.pause();
+  main_theme.currentTime = 0;
+  succes_sound.play();
+  // функция для того, чтобы можно было показывать результаты нашей печати
+  //мы просто получаем данные из LocalStorage, после этого мы их фильтруем соответсвующим образом и потом передаем в отрисовку. Звучит как что-то слишком сложное
+  localStorage.setItem(+new Date(), content); //функция для того, чтобы можно было показывать результаты любого их участников
+  (function drawOnLoad() {
+    let temp_arr = []; //создаем временный массив для того, чтобы разместить наши результаты
+    for (let i = 0; i < localStorage.length; i++) {
+      temp_arr.push(+localStorage.key(i)); 
+    }
+    temp_arr.sort(); // отсортировать наш массив результатов
+    for (let i = 0; i < temp_arr.length; i++) {
+      let item_time = new Date(temp_arr[i]); // получаем время, в которое у нас был получен результат
+      target_El.insertAdjacentHTML(
+        "afterend",
+        `<th>${item_time.getDate()} /${item_time.getMonth()}  ${item_time.getHours()} : ${item_time.getMinutes()} </th>
+              <th> ${localStorage.getItem(String(temp_arr[i]))}</th>
+              `
+      );
+    }
+  })();
+
+}
